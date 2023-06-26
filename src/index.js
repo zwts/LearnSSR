0c5bdd3b928270d74245b8fd21ff5538ff668bd2
Tan Shi <shi.tan@kaiostech.com>
2023-06-26 19:29:49 +0800

    hello ts

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const childProcess = require('child_process');
+
+const app  = express();
+
+app.get('*', (req, res) => {
+  res.send(`
+    <html>
+      <body>
+        <div>Hello ts</div>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('ssr server listen on 3000');
+});
+
+childProcess.exec('start http://172.0.0.1:3000');
