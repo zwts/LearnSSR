20a5cd62148f1ea999c140525e5d324df1c7481e
Tan Shi <shi.tan@kaiostech.com>
2023-07-06 19:01:04 +0800

    server link and client navigate

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+
   return (<
     div>
       <h1>Home title</h1>
       <button
-        onClick={() => {alert('This home page')}}
+        onClick={(): void => {alert('This home page')}}
       >
         Alert
       </button>
+      <a href="http://127.0.0.1:3000/demo">link to demo</a>
+      <span
+        onClick={(): void => {navigate('demo')}}
+      >
+        router to demo
+      </span>
     </div>
   );
 };
