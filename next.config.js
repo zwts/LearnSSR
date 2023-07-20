65f9f91dd5188eee19eddd3c3cd3a3d2c4652d05
Tan Shi <shi.tan@kaiostech.com>
2023-07-20 17:54:59 +0800

    initail commit, cearte app via nextjs

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const path = require("path");
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
+};
